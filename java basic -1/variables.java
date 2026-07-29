df411eaf87fd1ebd48dec845be3cbc405060f106
Ritika Sharma <sharmaritika5611@gmail.com>
2026-07-29 13:43:00 +0530

    Added Variables program

---
+++ java basic -1/variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[]args){
+
+        String name = "Ritika";
+        int age = 20;
+        double cgpa = 8.8;
+        char section = 'A';
+        boolean placementReady = true;
+
+        System.out.println("Name:" + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Section: " + section);
+        System.out.println("Placement Ready: " + placementReady);
+     }
+}
