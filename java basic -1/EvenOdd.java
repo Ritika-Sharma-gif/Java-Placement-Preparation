5b1b2fc49eabc7825b2ee78b779d42800ce617c3
Ritika Sharma <sharmaritika5611@gmail.com>
2026-08-01 14:01:47 +0530

    Added EvenOdd program using if-else

---
+++ java basic -1/EvenOdd.java
@@ -0,0 +1,25 @@
+import java.util.Scanner; 
+public class EvenOdd {
+    public static void main(String[]args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a nummber: ");
+        int number = sc.nextInt();
+
+        if(number % 2 == 0) {
+
+            System.out.println("The number is even");
+        } 
+        else {
+            System.out.println("The number is odd");
+
+        }
+         sc.close();
+
+        }
+
+
+
+
+    }
