ebbc946ac1886787090a5548336ae525fc5f8917
Ritika Sharma <sharmaritika5611@gmail.com>
2026-08-15 15:54:49 +0530

    Added LongestWord string program

---
+++ java basic -1/LongestWord.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LongestWord {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        String[] words = sentence.split("\\s+");
+
+        String longestWord = "";
+
+        for (String word : words) {
+
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+
+        System.out.println("Longest Word = " + longestWord);
+        System.out.println("Length = " + longestWord.length());
+
+        sc.close();
+    }
+}
