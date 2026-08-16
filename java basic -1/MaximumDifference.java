7afbcc53306453c8b54c5d372f839a309bbb594e
Ritika Sharma <sharmaritika5611@gmail.com>
2026-08-16 15:15:55 +0530

    Added MaximumDifference array program

---
+++ java basic -1/MaximumDifference.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class MaximumDifference {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int minElement = arr[0];
+        int maxDifference = arr[1] - arr[0];
+
+        for (int i = 1; i < n; i++) {
+
+            int difference = arr[i] - minElement;
+
+            if (difference > maxDifference) {
+                maxDifference = difference;
+            }
+
+            if (arr[i] < minElement) {
+                minElement = arr[i];
+            }
+        }
+
+        System.out.println("Maximum Difference = " + maxDifference);
+
+        sc.close();
+    }
+}
